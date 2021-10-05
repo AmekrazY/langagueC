62394b57e715f4b9e9b2375fb8f9ebe4c7ac86d5
Yassine Amekraz <90899222+AmekrazY@users.noreply.github.com>
2021-10-05 20:43:41 +0200

    Update foot.c

--- foot.c
+++ foot.c
@@ -1,38 +1,29 @@
-#include <stdio.h>
-#include <cs50.h>
-#include <string.h>
+#include<stdio.h>
+#include<cs50.h>
 
 int main(void)
-
 {
+string s[5][5] = {{"null","PSG","OMG","OL","SR"},
+{"PSG","null","2-1","1-3","0-1"},
+{"OMG","3-2","null","2-2","0-1"},
+{"OL","1-3","1-1","null","3-2"},
+{"SR","2-1","3-1","2-2","null"} };
+for (int i=1; i<5; i++)
+{
+string sum[5] = {"null","PSG","OMG","OL","SR"};
+printf ("\n Score of %s is: ", sum[i]);
+int total = 0;
+for (int j=1; j<5; j++)
+{
+if (i != j)
+{
+printf (" %c, %c,", s[i][j][0], s[j][i][2]);
+total = total + (s[i][j][0] -'0') + (s[j][i][2] -'0') ;
+}
 
-    int tab[3][3];
-    tab[0][0]=0;
-    tab[0][1]=2-1;
-    tab[0][2]=1-3;
-    tab[0][3]=0-1;
-    tab[1][0]=3-2;
-    tab[1][1]=0;
-    tab[1][2]=2-2;
-    tab[1][3]=0-1;
-    tab[2][0]=1-3;
-    tab[2][1]=1-1;
-    tab[2][2]=0;
-    tab[2][3]=3-";
-    tab[3][0]=2-1;
-    tab[3][1]=3-1;
-    tab[3][2]=2-2;
-    tab[3][3]=0;
-    //int l=strlen (tab[3][3]);
-    for ( int i = 0; i<3;i++)
-    {
-        for ( int j =0;j<3;i++)
-        {
-            printf("%i",tab[i][j]);
-        }
-        printf("\n");
-    }
-
-
+  }
+ 
+ printf("\n total %s is : %i \n", sum[i], total);
+}
 
 }
